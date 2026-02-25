301f32629aa1cc65e8d45d5a37de29e53b32e90e
Samuel <samuelruizmartin2412@gmail.com>
2026-02-25 20:02:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ejercicio 10
+Crea un mini-diccionario español-inglés que contenga, al menos, 20 palabras (con su correspondiente traducción). Utiliza un objeto de la clase HashMap para almacenar las parejas de palabras. El programa
---
+++ src/App.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        HashMap<String, String> diccionario = new HashMap<>(20);
+
+        diccionario.put("Gato", "Cat");
+        diccionario.put("Perro", "Dog");
+        diccionario.put("Casa", "House");
+        diccionario.put("Mesa", "Table");
+        diccionario.put("Comida", "Food");
+        diccionario.put("Madre ", "Mother");
+        diccionario.put("Gente", "People");
+        diccionario.put("Amigo", "Friend");
+        diccionario.put("Love", "Amor");
+        diccionario.put("Beber", "Drink");
+        diccionario.put("Comer", "Eat");
+        diccionario.put("Dormir", "Sleep");
+        diccionario.put("Trabajar", "Work");
+        diccionario.put("Manzana", "Apple");
+        diccionario.put("Leche", "Milk");
+        diccionario.put("Sol", "Sun");
+        diccionario.put("Luna", "Moon");
+        diccionario.put("Arbol", "Tree");
+        diccionario.put("Rojo", "Red");
+        diccionario.put("Amarillo", "Yellow");
+
+        for (String español : diccionario.keySet()) {
+            System.out.println(español + " - " + diccionario.get(español));
+        }
+    }
+}
